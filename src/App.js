d7431cd6fa9e0e2a92ee010f83c0541243240826
nithinshankar123 <snithin816@gmail.com>
2023-10-23 20:44:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Rtask1 from "./Components/Rtask1";
+
+function App() {
+  return (
+    <div className="App"><Rtask1/></div>
+  );
+}
+
+export default App;
